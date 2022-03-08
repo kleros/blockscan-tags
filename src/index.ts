8052d60ff4fde0f36cc6e9c4d24436e98d245bd7
greenlucid <40367733+greenlucid@users.noreply.github.com>
2022-03-08 14:12:36 +0100

    feat: support for xdai
    also, heavy refactoring and some extra
    validation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch'
 import { open } from 'lmdb' // or require
+import conf from './config'
 
 interface Prop {
   value: string
@@ -8,46 +9,85 @@ interface Tag {
   props: Prop[]
 }
 
+// CHAIN_ID === 1: production, else staging.
 const blockscanDB = open({
-  path: `db-${process.env.CHAIN_ID as string}`,
+  path: `db-${conf.CHAIN_ID}`,
 })
 
-async function init() {
+const tagsFromEndpoint = async (
+  subgraphEndpoint: string,
+  list: string,
+): Promise<Tag[]> => {
   const subgraphQuery = {
     query: `
       {
-        registered: litems(where: { registryAddress: "${(
-          process.env.LIST_ADDRESS as string
-        ).toLowerCase()}", status:  Registered }) {
+        registered: litems(where: { registryAddress: "${list.toLowerCase()}", status:  Registered }) {
           props {
             value
           }
         }
-        clearingRequested: litems(where: { registryAddress: "${(
-          process.env.LIST_ADDRESS as string
-        ).toLowerCase()}", status:  ClearingRequested }) {
+        clearingRequested: litems(where: { registryAddress: "${list.toLowerCase()}", status:  ClearingRequested }) {
           props {
             value
           }
         }
       }
-
     `,
   }
 
+  const response = await fetch(subgraphEndpoint, {
+    method: 'POST',
+    body: JSON.stringify(subgraphQuery),
+  })
+
+  const { data } = await response.json()
+  const { clearingRequested, registered } = data
+  const registeredTags: Tag[] = clearingRequested.concat(registered)
+  return registeredTags
+}
+
+const textDuplicates = async (
+  publicNameTag: string,
+  listAddress: string,
+  subgraphEndpoint: string,
+) => {
+  const duplicateQuery = `
+    {
+      itemSearch(text: "${listAddress} & '${publicNameTag
+    .trim()
+    .replace(' ', ' & ')}':*", status:  Registered) {
+        id
+      }
+    }
+  `
+  const response = await (
+    await fetch(subgraphEndpoint, {
+      method: 'POST',
+      body: JSON.stringify({
+        query: duplicateQuery,
+      }),
+    })
+  ).json()
+
+  const { data } = response
+  const { itemSearch: items } = data
+  return items
+}
+
+async function init() {
   let registeredTags: Tag[] = []
   try {
-    console.info(`Fetching registered tags from subgraph...`)
-    const response = await fetch(process.env.GTCR_SUBGRAPH_URL as string, {
-      method: 'POST',
-      body: JSON.stringify(subgraphQuery),
-    })
-    console.info('Done.')
-
-    const { data } = await response.json()
-
-    const { clearingRequested, registered } = data
-    registeredTags = registeredTags.concat(clearingRequested).concat(registered)
+    console.info(`Fetching registered tags from mainnet subgraph...`)
+    const mainnetTags = await tagsFromEndpoint(
+      conf.MAINNET_GTCR_SUBGRAPH_URL,
+      conf.MAINNET_LIST_ADDRESS,
+    )
+    console.info(`Fetching registered tags from xdai subgraph...`)
+    const xdaiTags = await tagsFromEndpoint(
+      conf.XDAI_GTCR_SUBGRAPH_URL,
+      conf.XDAI_LIST_ADDRESS,
+    )
+    registeredTags = registeredTags.concat(mainnetTags).concat(xdaiTags)
     console.info(`Got ${registeredTags.length} registered tags.`)
   } catch (error) {
     console.error(`Failed to fetch items`, error)
@@ -65,40 +105,51 @@ async function init() {
     publicNameTag = publicNameTag.slice(0, 35)
 
     if (!/^0x[a-fA-F0-9]{40}$/.test(address)) continue // Not an ETH address.
-    if (blockscanDB.get(address)) continue // Tag already posted to API.
+    if (blockscanDB.get(address) || blockscanDB.get(address.toLowerCase()))
+      continue // Tag already posted to API.
     if (publicNameTag.length > 35) continue // Exceeds max length.
     if (publicNote.length === 0) continue // Mandatory field.
 
-    const duplicateQuery = `
-      {
-        itemSearch(text: "${process.env.LIST_ADDRESS} & '${publicNameTag
-      .trim()
-      .replace(' ', ' & ')}':*", status:  Registered) {
-          id
-        }
-      }
-    `
-    const response = await (
-      await fetch(process.env.GTCR_SUBGRAPH_URL as string, {
-        method: 'POST',
-        body: JSON.stringify({
-          query: duplicateQuery,
-        }),
-      })
-    ).json()
+    try {
+      const mainnetDupes = await textDuplicates(
+        publicNameTag,
+        conf.MAINNET_GTCR_SUBGRAPH_URL,
+        conf.MAINNET_LIST_ADDRESS,
+      )
+      const xdaiDupes = await textDuplicates(
+        publicNameTag,
+        conf.XDAI_GTCR_SUBGRAPH_URL,
+        conf.XDAI_LIST_ADDRESS,
+      )
 
-    const { data } = response
-    const { itemSearch: items } = data
+      if (mainnetDupes.length + xdaiDupes.length > 1) continue // Duplicate, ignore.
+    } catch (error) {
+      console.error(
+        `Failed to check dupes for name tag ${publicNameTag}`,
+        error,
+      )
+      continue
+    }
 
-    if (items.length > 1) continue // Duplicate, ignore.
+    /**
+     * You can't check if an address is dupe, because it's saved as string
+     * It could have a different lower case and upper case combo
+     */
 
     try {
       const query = `
-          https://repaddr.blockscan.com/reportaddressapi?apikey=${process.env.API_KEY}&address=${address}&chain=ETH&actiontype=1&customname=${publicNameTag}&comment=${publicNote}&infourl=${website}        `
-
-      const resp = await fetch(query)
-      console.info(await resp.json())
-      blockscanDB.put(address, true)
+          https://repaddr.blockscan.com/reportaddressapi?apikey=${conf.API_KEY}&address=${address}&chain=ETH&actiontype=1&customname=${publicNameTag}&comment=${publicNote}&infourl=${website}`
+      if (conf.CHAIN_ID === '1') {
+        const resp = await fetch(query)
+        console.info(await resp.json())
+        blockscanDB.put(address.toLowerCase(), true)
+      } else {
+        console.info(
+          `CHAIN_ID is ${conf.CHAIN_ID} so we're staging. Query and address saved would've been:`,
+        )
+        console.info(query)
+        console.info(address.toLowerCase())
+      }
     } catch (error) {
       console.error(`Failed to post tag for ${address}.`, error)
     }
